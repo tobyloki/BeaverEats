4aeab3d33f3a0b774ccd8bcfd77fe1f45a9afc1a
theusaf <theusafyt@gmail.com>
2022-10-07 19:58:59 -0700

    chore: add dockerfile to git

--- src/routers/locations.ts
+++ src/routers/locations.ts
@@ -3,20 +3,6 @@ import databaseUtil from "../util/db/index.js";
 
 export const locationsRouter = express.Router();
 
-const demoData = [
-  {
-    name: "Trader Bing's",
-    area: "Austin Hall",
-    address: "2751 SW Jefferson Way Corvallis, OR 97331",
-    description: "Example description",
-    latitude: 44.56505,
-    longitude: -123.282096,
-    usesDiningDollars: true,
-    startHours: "07:30",
-    endHours: "18:00",
-  },
-];
-
 function waitForDatabase(): Promise<void> {
   return new Promise((resolve, reject) => {
     let tries = 0;
